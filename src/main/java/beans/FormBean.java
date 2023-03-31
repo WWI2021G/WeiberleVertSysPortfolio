81adad9129c082cc48b5b48ef319c0003bd93fac
Weiberle17 <maxweiberle63@gmail.com>
2023-03-31 13:22:27 +0200

    Initial FormBean setup
    First try at managing the data. Mainly just setters and getters for each
    variable.

---
+++ src/main/java/beans/FormBean.java
@@ -0,0 +1,93 @@
+package beans;
+
+public class FormBean {
+  private int tischNr, anzahlCola, anzahlFanta, anzahlSprite, anzahlWasser, anzahlapfel, anzahlSchnitzel, anzahlKäsespätzle, anzahlSpaghetti, anzahlChicken, anzahlPommes;
+
+  public int getTischNr() {
+    return tischNr;
+  }
+
+  public void setTischNr(int tischNr) {
+    this.tischNr = tischNr;
+  }
+
+  public int getAnzahlCola() {
+    return anzahlCola;
+  }
+
+  public void setAnzahlCola(int anzahlCola) {
+    this.anzahlCola = anzahlCola;
+  }
+
+  public int getAnzahlFanta() {
+    return anzahlFanta;
+  }
+
+  public void setAnzahlFanta(int anzahlFanta) {
+    this.anzahlFanta = anzahlFanta;
+  }
+
+  public int getAnzahlSprite() {
+    return anzahlSprite;
+  }
+
+  public void setAnzahlSprite(int anzahlSprite) {
+    this.anzahlSprite = anzahlSprite;
+  }
+
+  public int getAnzahlWasser() {
+    return anzahlWasser;
+  }
+
+  public void setAnzahlWasser(int anzahlWasser) {
+    this.anzahlWasser = anzahlWasser;
+  }
+
+  public int getAnzahlapfel() {
+    return anzahlapfel;
+  }
+
+  public void setAnzahlapfel(int anzahlapfel) {
+    this.anzahlapfel = anzahlapfel;
+  }
+
+  public int getAnzahlSchnitzel() {
+    return anzahlSchnitzel;
+  }
+
+  public void setAnzahlSchnitzel(int anzahlSchnitzel) {
+    this.anzahlSchnitzel = anzahlSchnitzel;
+  }
+
+  public int getAnzahlKäsespätzle() {
+    return anzahlKäsespätzle;
+  }
+
+  public void setAnzahlKäsespätzle(int anzahlKäsespätzle) {
+    this.anzahlKäsespätzle = anzahlKäsespätzle;
+  }
+
+  public int getAnzahlSpaghetti() {
+    return anzahlSpaghetti;
+  }
+
+  public void setAnzahlSpaghetti(int anzahlSpaghetti) {
+    this.anzahlSpaghetti = anzahlSpaghetti;
+  }
+
+  public int getAnzahlChicken() {
+    return anzahlChicken;
+  }
+
+  public void setAnzahlChicken(int anzahlChicken) {
+    this.anzahlChicken = anzahlChicken;
+  }
+
+  public int getAnzahlPommes() {
+    return anzahlPommes;
+  }
+
+  public void setAnzahlPommes(int anzahlPommes) {
+    this.anzahlPommes = anzahlPommes;
+  }
+}
