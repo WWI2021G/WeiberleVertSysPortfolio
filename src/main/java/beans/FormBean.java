8c08c216e75ceeace4da95e0a0b3bc503256e10e
Weiberle17 <maxweiberle63@gmail.com>
2023-04-04 10:48:37 +0200

    New way of saving variables.
    Data now saved in multi dimensional array in order to save the same variables for multiple tables.

--- src/main/java/beans/FormBean.java
+++ src/main/java/beans/FormBean.java
@@ -1,7 +1,9 @@
 package beans;
 
 public class FormBean {
-  private int tischNr, anzahlCola, anzahlFanta, anzahlSprite, anzahlWasser, anzahlapfel, anzahlSchnitzel, anzahlKäsespätzle, anzahlSpaghetti, anzahlChicken, anzahlPommes;
+  private int tischNr;
+
+  int[][] tische = new int[5][10];
 
   public int getTischNr() {
     return tischNr;
@@ -12,82 +14,82 @@ public class FormBean {
   }
 
   public int getAnzahlCola() {
-    return anzahlCola;
+    return tische[tischNr - 1][0];
   }
 
   public void setAnzahlCola(int anzahlCola) {
-    this.anzahlCola = anzahlCola;
+    tische[tischNr - 1][0] = anzahlCola;
   }
 
   public int getAnzahlFanta() {
-    return anzahlFanta;
+    return tische[tischNr - 1][1];
   }
 
   public void setAnzahlFanta(int anzahlFanta) {
-    this.anzahlFanta = anzahlFanta;
+    tische[tischNr - 1][1] = anzahlFanta;
   }
 
   public int getAnzahlSprite() {
-    return anzahlSprite;
+    return tische[tischNr - 1][2];
   }
 
   public void setAnzahlSprite(int anzahlSprite) {
-    this.anzahlSprite = anzahlSprite;
+    tische[tischNr - 1][2] = anzahlSprite;
   }
 
   public int getAnzahlWasser() {
-    return anzahlWasser;
+    return tische[tischNr - 1][3];
   }
 
   public void setAnzahlWasser(int anzahlWasser) {
-    this.anzahlWasser = anzahlWasser;
+    tische[tischNr - 1][3] = anzahlWasser;
   }
 
-  public int getAnzahlapfel() {
-    return anzahlapfel;
+  public int getAnzahlApfel() {
+    return tische[tischNr - 1][4];
   }
 
-  public void setAnzahlapfel(int anzahlapfel) {
-    this.anzahlapfel = anzahlapfel;
+  public void setAnzahlApfel(int anzahlApfel) {
+    tische[tischNr - 1][4] = anzahlApfel;
   }
 
   public int getAnzahlSchnitzel() {
-    return anzahlSchnitzel;
+    return tische[tischNr - 1][5];
   }
 
   public void setAnzahlSchnitzel(int anzahlSchnitzel) {
-    this.anzahlSchnitzel = anzahlSchnitzel;
+    tische[tischNr - 1][5] = anzahlSchnitzel;
   }
 
-  public int getAnzahlKäsespätzle() {
-    return anzahlKäsespätzle;
+  public int getAnzahlKaesespaetzle() {
+    return tische[tischNr - 1][6];
   }
 
-  public void setAnzahlKäsespätzle(int anzahlKäsespätzle) {
-    this.anzahlKäsespätzle = anzahlKäsespätzle;
+  public void setAnzahlKaesespaetzle(int anzahlKaesespaetzle) {
+    tische[tischNr - 1][6] = anzahlKaesespaetzle;
   }
 
   public int getAnzahlSpaghetti() {
-    return anzahlSpaghetti;
+    return tische[tischNr - 1][7];
   }
 
   public void setAnzahlSpaghetti(int anzahlSpaghetti) {
-    this.anzahlSpaghetti = anzahlSpaghetti;
+    tische[tischNr - 1][7] = anzahlSpaghetti;
   }
 
   public int getAnzahlChicken() {
-    return anzahlChicken;
+    return tische[tischNr - 1][8];
   }
 
   public void setAnzahlChicken(int anzahlChicken) {
-    this.anzahlChicken = anzahlChicken;
+    tische[tischNr - 1][8] = anzahlChicken;
   }
 
   public int getAnzahlPommes() {
-    return anzahlPommes;
+    return tische[tischNr - 1][9];
   }
 
   public void setAnzahlPommes(int anzahlPommes) {
-    this.anzahlPommes = anzahlPommes;
+    tische[tischNr - 1][9] = anzahlPommes;
   }
 }
