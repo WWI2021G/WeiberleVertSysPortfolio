eba176a812bbc37adb1a2fc4d175acaf4ae809c7
Weiberle17 <maxweiberle63@gmail.com>
2023-04-14 17:12:06 +0200

    Added isOrderOpen function
    Function returns true if there is a open order for this table, false if
    there isn't.

--- src/main/java/beans/FormBean.java
+++ src/main/java/beans/FormBean.java
@@ -92,4 +92,13 @@ public class FormBean {
   public void setAnzahlPommes(int anzahlPommes) {
     tische[tischNr - 1][9] = anzahlPommes;
   }
+
+  public boolean isOrderOpen() {
+    for (int i = 0; i < tische[tischNr - 1].length; i++) {
+      if (tische[tischNr -1 ][i] != 0) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
